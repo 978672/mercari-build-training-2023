333fe2144de1743457a143873f3648a92cc99ea1
kae <maple.mr08@gmail.com>
2023-05-13 23:51:41 +0900

    4 & 5

--- go/app/main.go
+++ go/app/main.go
@@ -9,6 +9,7 @@ import (
 	"path"
 	"strings"
 	"crypto/sha256"
+	"strconv"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -38,6 +39,14 @@ func root(c echo.Context) error {
 	return c.JSON(http.StatusOK, res)
 }
 
+/* 3.4 
+curl -X POST 
+--url http://localhost:9000/items 
+-F name=jacket 
+-F category=fashion 
+-F image=@./local_image.jpg
+*/
+
 //ハッシュ化
 func getSHA256Binary(s string) []byte {
     r := sha256.Sum256([]byte(s))
@@ -48,13 +57,18 @@ func addItem(c echo.Context) error {
 	// Get form data
 	name := c.FormValue("name")
 	category := c.FormValue("category")
-	imageName := c.FormValue("image")
-	
-	hashedImg := getSHA256Binary(imageName)
+	image, err := c.FormFile("image")
+	if err != nil {
+		return err
+	}
+
+	//hashしたものに".jpg"をつける
+	hashedImg := getSHA256Binary(image.Filename)
 	image_filename := fmt.Sprintf("%x%s",hashedImg , ".jpg")
 
 	c.Logger().Infof("Receive item: %s", name)
 	c.Logger().Infof("Receive item: %s", category)
+	c.Logger().Infof("Receive item: %s", image.Filename)
 
 	// 新しいitemを作る
 	var newItem Item
@@ -125,6 +139,37 @@ func getImg(c echo.Context) error {
 	return c.File(imgPath)
 }
 
+
+func getItemByID(c echo.Context)  error{
+	jsonFile, err := os.Open("items.json")
+	if err != nil {
+		fmt.Println("JSONファイルを開けません", err)
+		return c.JSON(http.StatusInternalServerError, err)
+	}
+	defer jsonFile.Close()
+	jsonData, err := ioutil.ReadAll(jsonFile)
+	if err != nil {
+		fmt.Println("JSONファイルを開けません", err)
+		return c.JSON(http.StatusInternalServerError, err)
+	}
+
+	var items Items
+	json.Unmarshal(jsonData, &items)
+
+	id := c.Param("item_id") 
+	
+	idItem := Item{}
+	for index, element := range items.Items {
+		if(strconv.Itoa(index) == id){
+			idItem = element
+			return c.JSON(http.StatusOK, idItem)
+		}
+	}
+	res := Response{Message: "Not found"}
+	return c.JSON(http.StatusNotFound, res)
+}
+
+
 func main() {
 	e := echo.New()
 
@@ -147,6 +192,7 @@ func main() {
 	e.POST("/items", addItem)
 	e.GET("/items", getItem)
 	e.GET("/image/:imageFilename", getImg)
+	e.GET("/items/:item_id", getItemByID)
 
 	// Start server
 	e.Logger.Fatal(e.Start(":9000"))
